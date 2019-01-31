74f1da6de121d64beb317a2513226704138f4f48
Anna Tropnikova <anna@streetography.com>
2019-01-31 15:41:49 -0800

    a func to test your funcs

--- exam/lvl1/ft_strcpy.c
+++ exam/lvl1/ft_strcpy.c
@@ -14,3 +14,20 @@ char 	*ft_strcpy(char *s1, char *s2)
 	*dest = '\0';
 	return (dest);
 }
+
+/* a main to test your func follows. make sure to initialize with s1[] 
+below to null terminate the array. if you initialize with *s1 
+and then printf you may get bus error */
+
+#include <stdio.h>
+
+int	main()
+{
+	char s1[] = "hello";
+	char s2[] = "sup";
+
+	printf("%s\n", s1);
+	ft_strcpy(s1, s2);
+	printf("%s\n", s1);
+	return (0);
+}
