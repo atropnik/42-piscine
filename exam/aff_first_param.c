2863db550d0719d11f8672fcd2f17db3684ae685
atropnik <atropnikova@gmail.com>
2019-01-31 04:38:09 -0800

    lvl 0 complete

---
+++ exam/aff_first_param.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_first_param.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atropnik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/31 04:24:16 by atropnik          #+#    #+#             */
+/*   Updated: 2019/01/31 04:30:55 by atropnik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc < 2)
+		;
+	else
+		ft_putstr(argv[1]);
+	ft_putchar('\n');
+	return (0);
+}
---
+++ exam/aff_last_param.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_last_param.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atropnik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/31 04:33:58 by atropnik          #+#    #+#             */
+/*   Updated: 2019/01/31 04:36:06 by atropnik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	if (argc < 2)
+		;
+	else
+	{
+		i = argc - 1;
+		ft_putstr(argv[i]);
+	}
+	ft_putchar('\n');
+	return (0);
+}
