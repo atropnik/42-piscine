0f53df959733cabd72ce275a255c00d9eff42d4f
atropnik <atropnikova@gmail.com>
2019-02-01 01:52:25 -0800

    dont worry about meming -2147483648

--- exam/lvl2/ft_atoi.c
+++ exam/lvl2/ft_atoi.c
@@ -1,26 +1,33 @@
-int	ft_atoi(const char *str)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atropnik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/01 01:35:28 by atropnik          #+#    #+#             */
+/*   Updated: 2019/02/01 01:51:09 by atropnik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_atoi(const char *str)
 {
 	int sign;
 	int total;
 	int i;
 
+	i = 0;
 	sign = 1;
+	total = 0;
 	while (str[i] != '\0')
 	{
+		if (str[i] <= 32 || str[i] == '+')
+			;
 		if (str[i] == '-')
-		{
-			sign = -sign;
-			i++;
-		}
-		if (str[i] == '+')
-		{
-			i++;
-		}
-		while (str[i] >= '0' && str[i] <= '9')
-		{
-			total = total * 10 + str[i] - 48;
-			i++;
-		}
+			sign = -1 * sign;
+		if (str[i] >= 48 && str[i] <= 57)
+			total = total * 10 + (str[i] - 48);
+		i++;
 	}
 	return (sign * total);
 }
@@ -31,7 +38,7 @@ int	ft_atoi(const char *str)
 
 int	main()
 {
-	char str[] = "9999";
+	char str[] = "    \n\n\v\f\r\t -5234AAAgreghrsth";
 	printf("%d\n", ft_atoi(str));
 	return (0);
 }
