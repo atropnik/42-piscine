6c00c7c8d4fd6840699a174b1c104a0fb7957077
atropnik <atropnikova@gmail.com>
2019-02-15 01:46:07 -0800

    strdup checks for errors

--- exam/lvl2/ft_strdup.c
+++ exam/lvl2/ft_strdup.c
@@ -6,31 +6,30 @@
 /*   By: atropnik <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/01 03:29:39 by atropnik          #+#    #+#             */
-/*   Updated: 2019/02/01 03:47:53 by atropnik         ###   ########.fr       */
+/*   Updated: 2019/02/15 01:45:37 by atropnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strdup(char *src)
+char 	*ft_strdup(char *s1)
 {
 	char	*new_str;
 	int		i;
 
 	i = 0;
-	while (src[i] != '\0')
-		i++;
-	new_str = (char *)malloc(sizeof(char) * i + 1);
-	i = 0;
-	while (src[i] != '\0')
-	{
-		new_str[i] = src[i];
-		i++;
-	}
+	while (s1[i] != '\0')
+		++i;
+	if (!(new_str = (char *)malloc(sizeof(char) * i + 1)))
+		return (NULL);
 	new_str[i] = '\0';
-	return (new_str);
+	while (i >= 0)
+	{
+		new_str[i] = s1[i];
+		i--;
+	}
+	return(new_str);
 }
-
 // testing
 
 #include <stdio.h>
