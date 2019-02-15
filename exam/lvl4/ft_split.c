0f327bdb965336cd868aca4affc3e3defb8c3726
atropnik <atropnikova@gmail.com>
2019-02-15 04:00:40 -0800

    split

---
+++ exam/lvl4/ft_split.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atropnik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/15 03:59:55 by atropnik          #+#    #+#             */
+/*   Updated: 2019/02/15 04:00:12 by atropnik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char		**ft_split(char *str)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**split;
+
+	i = 0;
+	k = 0;
+	if (!(split = (char **)malloc(sizeof(char *) * 256)))
+		return (NULL);
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		i += 1;
+	while (str[i])
+	{
+		j = 0;
+		if (!(split[k] = (char *)malloc(sizeof(char) * 4096)))
+			return (NULL);
+		while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i])
+			split[k][j++] = str[i++];
+		while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+			i += 1;
+		split[k][j] = '\0';
+		k += 1;
+	}
+	split[k] = NULL;
+	return (split);
+}
